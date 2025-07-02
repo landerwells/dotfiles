e5fae0742eb1183ca9a0f77ec5fab1731cbcb726
landerwells <landerwells@gmail.com>
2025-07-01 22:54:38 -0500

    doom

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -32,13 +32,19 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-;; (setq doom-theme 'doom-one)
 (setq doom-theme 'doom-gruvbox)
-;; (setq doom-theme 'almost-mono-cream)
-
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type `relative)
+(setq company-minimum-prefix-length 1)
+(setq scroll-margin 8) ;; or whatever number you want
+
+(map! :n "C-d" (cmd! (evil-scroll-down nil) (recenter))
+      :n "C-u" (cmd! (evil-scroll-up nil) (recenter)))
+
+(map! :n "gj" 'evil-next-visual-line
+      :n "gk" 'evil-previous-visual-line)
+
+(map! :leader
+      :desc "Toggle Olivetti mode" "z" #'olivetti-mode)
 
 ;; (defun tag-new-node-as-draft ()
 ;;   (org-roam-tag-add '("draft")))
@@ -70,27 +76,17 @@
                     "#+title: ${title}\n#+date: %<%B %d, %Y %I:%M %p>\n")
          :immediate-finish t
          :unnarrowed t)
-        ("x" "indexes" plain "%?"
-         :if-new (file+head "indexes/${slug}.org"
+        ("b" "blog" plain "%?"
+         :if-new
+         (file+head "blog/${title}.org"
+                    "#+title: ${title}\n#+date: %<%B %d, %Y %I:%M %p>\n")
+         :immediate-finish t
+         :unnarrowed t)
+        ("x" "index" plain "%?"
+         :if-new (file+head "index/${slug}.org"
                             "#+title: ${title}\n#+date: %<%B %d, %Y %I:%M %p>\n")
          :immediate-finish t
          :unnarrowed t)))
-
-(use-package! org-roam
-  ;; :ensure t
-  :init
-  ;; (setq org-roam-directory "~/org-roam/")
-  ;; other Org-roam settings
-  :config
-  (with-eval-after-load 'ox-html
-    (defun my/org-html--details (details contents info)
-      "Export DETAILS block as a folded <details> element by default."
-      (let ((summary (org-element-property :summary details)))
-        (format "<details>%s%s</details>"
-                (if summary (format "<summary>%s</summary>" summary) "")
-                contents))))
-
-  (advice-add 'org-html--details :override #'my/org-html--details))
 
 ;; (use-package! org-roam-ui
 ;;   :after org-roam ;; or :after org
@@ -142,33 +138,7 @@
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
 
-(map! :n "C-d" (cmd! (evil-scroll-down nil) (recenter))
-      :n "C-u" (cmd! (evil-scroll-up nil) (recenter)))
-
-(map! :n "gj" 'evil-next-visual-line
-      :n "gk" 'evil-previous-visual-line)
-
-;; (map! "C-b" #'neotree-toggle)
-
-;; I would also like to add the ability to center when searching
-
-(map! :leader
-      :desc "Toggle Olivetti mode" "z" #'olivetti-mode)
-
-;; I don't want line wrap on files that are not markdown or org.
-(defun my-disable-line-wrap ()
-  "Disable line wrap except for Org and Markdown modes."
-  (unless (or (derived-mode-p 'org-mode 'markdown-mode))
-    (setq truncate-lines t)))  ;; Disable line wrapping
-
-(add-hook 'find-file-hook #'my-disable-line-wrap)
-
-;; Set the fill column to 80 by default
-(setq fill-column 80)
-
-;; Exclude .md and .org files from having the fill column set
-;; (add-hook 'markdown-mode-hook (lambda () (setq fill-column nil)))
-;; (add-hook 'org-mode-hook (lambda () (setq fill-column nil)))
+;; create a function to disalbe line wrap on files other than markdown or org
 
 (after! evil
   ;; Bind Ctrl+h, Ctrl+j, Ctrl+k, Ctrl+l directly for window navigation
@@ -184,26 +154,53 @@
 (setq org-export-with-date nil)
 (setq org-export-timestamp-file nil)
 
-;; Need to come up with a keybind for spellchecking, the current on is ass
+; (defun my/org-roam-random-draft-note ()
+;   "Open a random Org-roam note tagged with :draft:."
+;   (interactive)
+;   (let* ((draft-nodes (org-roam-db-query
+;                        [:select [id file title]
+;                                 :from tags
+;                                 :inner :join nodes :on (= tags.node-id nodes.id)
+;                                 :where (= tag "draft")]))
+;          (count (length draft-nodes)))
+;     (if (> count 0)
+;         (let* ((selected (nth (random count) draft-nodes))
+;                (file (nth 1 selected))
+;                (title (nth 2 selected)))
+;           (message "Opening draft note: %s" title)
+;           (find-file file))
+;       (message "No draft notes found."))))
 
-(setq company-minimum-prefix-length 1)
-(setq scroll-margin 8) ;; or whatever number you want
+(setq org-publish-project-alist
+      '(("org"
+         :base-directory "~/org/roam/blog/"
+         :publishing-function org-html-publish-to-html
+         :publishing-directory "~/Developer/landerwells.github.io/blog/"
 
-;; (setq treesit--enabled-p nil)
+         :section-numbers nil
+         :with-toc nil
 
-(defun my/org-roam-random-draft-note ()
-  "Open a random Org-roam note tagged with :draft:."
-  (interactive)
-  (let* ((draft-nodes (org-roam-db-query
-                       [:select [id file title]
-                                :from tags
-                                :inner :join nodes :on (= tags.node-id nodes.id)
-                                :where (= tag "draft")]))
-         (count (length draft-nodes)))
-    (if (> count 0)
-        (let* ((selected (nth (random count) draft-nodes))
-               (file (nth 1 selected))
-               (title (nth 2 selected)))
-          (message "Opening draft note: %s" title)
-          (find-file file))
-      (message "No draft notes found."))))
+         :recursive t                ;; Publish files in subdirectories
+         :headline-levels 4          ;; A reasonable default for post structure
+         :auto-sitemap t             ;; IMPORTANT: This generates a list of all posts
+         :sitemap-filename "index.html" ;; Name the sitemap file "index.html"
+         :sitemap-title "Blog"       ;; The title for the main blog page
+         :sitemap-sort-files anti-chronologically
+
+         ;; This adds your stylesheet to every generated post.
+         ;; :html-head "<link rel=\"stylesheet\" type=\"text/css\" href=\"../style.css\" /> <link rel=\"shortcut icon\" type=\"image/x-icon\" href=\"../img/duck.png\"/>"
+         ;; :html-preamble t ;; Adds a standard preamble
+         ;; :html-postamble t ;; Adds a standard postamble
+         )))
+
+(defun my-publish-git-push ()
+  "Commit and push website changes after publishing."
+  (let ((default-directory "~/Developer/landerwells.github.io/"))
+    (start-process-shell-command
+     "git-publish"
+     "*git-publish-output*"
+     "git add . && git commit -m 'Publish' && git push")))
+
+(add-hook 'org-publish-after-all-publishing-hook #'my-publish-git-push)
+
+;; I wonder if it is possible to have a script run based off a hook into a function
--- .doom.d/init.el
+++ .doom.d/init.el
@@ -44,7 +44,7 @@
        ophints           ; highlight the region an operation acts on
        (popup +defaults)   ; tame sudden yet inevitable temporary windows
        ;;tabs              ; a tab bar for Emacs
-       treemacs          ; a project drawer, like neotree but cooler
+       ;; treemacs          ; a project drawer, like neotree but cooler
        ;;unicode           ; extended unicode support for various languages
        (vc-gutter +pretty) ; vcs diff in the fringe
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
@@ -114,7 +114,7 @@
        :lang
        ;;agda              ; types of types of types of types...
        ;;beancount         ; mind the GAAP
-       (cc +lsp)         ; C > C++ == 1
+       ;;(cc +lsp)         ; C > C++ == 1
        ;;clojure           ; java with a lisp
        ;;common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
@@ -123,9 +123,9 @@
        ;;data              ; config/data formats
        ;;(dart +flutter)   ; paint ui and not much else
        ;;dhall
-       elixir            ; erlang done right
+       ;;elixir            ; erlang done right
        ;;elm               ; care for a cup of TEA?
-       emacs-lisp        ; drown in parentheses
+       ;;emacs-lisp        ; drown in parentheses
        ;;erlang            ; an elegant language for a more civilized age
        ;;ess               ; emacs speaks statistics
        ;;factor
@@ -148,7 +148,7 @@
        ;;lean              ; for folks with too much to prove
        ;;ledger            ; be audit you can be
        ;;lua               ; one-based indices? one-based indices
-       markdown          ; writing docs for people to ignore
+       ;;markdown          ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
@@ -158,22 +158,22 @@
        ;;plantuml          ; diagrams for confusing people more
        ;;graphviz          ; diagrams for confusing yourself even more
        ;;purescript        ; javascript, but functional
-       python            ; beautiful is better than ugly
+       ;;python            ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
        ;;rest              ; Emacs as a REST client
        ;;rst               ; ReST in peace
        ;;(ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
-       ;; (rust +lsp)       ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+       ;;(rust +lsp)       ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
-       (scheme +guile)   ; a fully conniving family of lisps
-       sh                ; she sells {ba,z,fi}sh shells on the C xor
+       ;;(scheme +guile)   ; a fully conniving family of lisps
+       ;;sh                ; she sells {ba,z,fi}sh shells on the C xor
        ;;sml
        ;;solidity          ; do you need a blockchain? No.
        ;;swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
-       ;; (web +lsp)               ; the tubes
+       ;;(web +lsp)               ; the tubes
        ;;yaml              ; JSON, but readable
        ;;zig               ; C, but simpler
 
