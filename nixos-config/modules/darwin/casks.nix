6b8ae06a7abe5a90084693d511c185662effcde7
landerwells <landerwells@gmail.com>
2025-07-21 09:11:25 -0500

    nixos-config

---
+++ nixos-config/modules/darwin/casks.nix
@@ -0,0 +1,25 @@
+_:
+
+[
+  # Development Tools
+  "homebrew/cask/docker"
+  "visual-studio-code"
+  "emacs-mac"
+
+  # Communication Tools
+  "discord"
+
+  # Utility Tools
+  "syncthing"
+
+  # Entertainment Tools
+  "vlc"
+
+  # Productivity Tools
+  "raycast"
+
+  # Browsers
+  "google-chrome"
+
+  "nikitabobko/tap/aerospace"
+]
