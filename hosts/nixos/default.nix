e806f52aaa0e4d63ba1ac57ff8dbe32886371d15
landerwells <landerwells@gmail.com>
2026-02-20 18:25:41 -0600

    NixOS: Build generation 241 on 2026-02-20 at 18:25:39

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -49,9 +49,7 @@ in {
 
   networking = {
     networkmanager.enable = true;
-    # networkmanager.dns = "none"; # Prevent NetworkManager from overriding DNS
     hostName = "nixos";
-    # nameservers = ["127.0.0.1"]; # Use AdGuard Home for DNS
     firewall = {
       enable = true;
       allowedTCPPorts = [53 3003]; # DNS over TCP + AdGuard Home web interface
@@ -115,39 +113,32 @@ in {
   services = {
     adguardhome = {
       enable = true;
-      # You can select any ip and port, just make sure to open firewalls where needed
-      host = "127.0.0.1";
+      host = "0.0.0.0";
       port = 3003;
       settings = {
         dns = {
+          bind_hosts = ["0.0.0.0"];
+          port = 53;
           upstream_dns = [
-            # Example config with quad9
-            "9.9.9.9#dns.quad9.net"
-            "149.112.112.112#dns.quad9.net"
-            # Uncomment the following to use a local DNS service (e.g. Unbound)
-            # Additionally replace the address & port as needed
-            # "127.0.0.1:5335"
+            "9.9.9.9"
+            "149.112.112.112"
           ];
         };
         filtering = {
           protection_enabled = true;
           filtering_enabled = true;
-
-          parental_enabled = false; # Parental control-based DNS requests filtering.
+          parental_enabled = false;
           safe_search = {
-            enabled = false; # Enforcing "Safe search" option for search engines, when possible.
+            enabled = false;
           };
         };
-        # The following notation uses map
-        # to not have to manually create {enabled = true; url = "";} for every filter
-        # This is, however, fully optional
         filters =
           map (url: {
             enabled = true;
             url = url;
           }) [
-            "https://adguardteam.github.io/HostlistsRegistry/assets/filter_9.txt" # The Big List of Hacked Malware Web Sites
-            "https://adguardteam.github.io/HostlistsRegistry/assets/filter_11.txt" # malicious url blocklist
+            "https://adguardteam.github.io/HostlistsRegistry/assets/filter_9.txt"
+            "https://adguardteam.github.io/HostlistsRegistry/assets/filter_11.txt"
           ];
       };
     };
