e98cbf93e4f2264ecc9506efbc97ee992c699c59
landerwells <landerwells@gmail.com>
2026-03-07 15:44:05 -0600

    NixOS: Build generation 260 on 2026-03-07 at 11:23:27

--- config/doom/init.el
+++ config/doom/init.el
@@ -14,9 +14,6 @@
 ;;      Alternatively, press 'gd' (or 'C-c c d') on a module to browse its
 ;;      directory (for easy access to its source code).
 
-(when (daemonp)
-  (exec-path-from-shell-initialize))
-
 (doom! :input
        ;;bidi              ; (tfel ot) thgir etirw uoy gnipleh
        ;;chinese
