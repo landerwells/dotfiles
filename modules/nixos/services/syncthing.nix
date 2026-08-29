c1245f8b7d2f66a003da4fb21edd4c835dfb08b9
landerwells <landerwells@gmail.com>
2026-08-29 18:14:25 -0500

    NixOS: Build generation 322 on 2026-08-29 at 18:14:23

--- modules/nixos/services/syncthing.nix
+++ modules/nixos/services/syncthing.nix
@@ -10,6 +10,7 @@
       devices = {
         "macos" = {id = "JFSZU24-XA7JTXO-ZMZXO4L-KBDSOMA-2M74Y4X-GSO3EMF-YSMFDRR-AJG4XAW";};
         "hisense" = {id = "S4IKJUJ-NDJOT55-CHXXW4J-LKNWGSE-WATITGO-6YHWQZH-QI5TIXR-SBDFQQS";};
+        "fugu" = {id = "LTTABM2-KKXQKML-DHFHXLP-AOUGAG5-3UYTKPI-3RDHEYY-H5XZYNF-Q6LWXQT";};
       };
       folders = {
         "Books" = {
@@ -21,9 +22,9 @@
           devices = ["macos"];
           ignorePerms = false;
         };
-        "org" = {
-          path = "/home/landerwells/org";
-          devices = ["macos"];
+        "notes" = {
+          path = "/home/landerwells/notes";
+          devices = ["macos" "fugu"];
         };
       };
     };
