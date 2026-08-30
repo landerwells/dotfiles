3485408ed2c3b1773ec6762695cfa885cd364587
landerwells <landerwells@gmail.com>
2026-08-30 08:48:13 -0500

    NixOS: Build generation 323 on 2026-08-30 at 08:48:10

--- modules/nixos/services/syncthing.nix
+++ modules/nixos/services/syncthing.nix
@@ -10,7 +10,7 @@
       devices = {
         "macos" = {id = "JFSZU24-XA7JTXO-ZMZXO4L-KBDSOMA-2M74Y4X-GSO3EMF-YSMFDRR-AJG4XAW";};
         "hisense" = {id = "S4IKJUJ-NDJOT55-CHXXW4J-LKNWGSE-WATITGO-6YHWQZH-QI5TIXR-SBDFQQS";};
-        "fugu" = {id = "LTTABM2-KKXQKML-DHFHXLP-AOUGAG5-3UYTKPI-3RDHEYY-H5XZYNF-Q6LWXQT";};
+        "fugu" = {id = "MXIZ437-CSW4RAW-FERBO63-OSRBEC6-2VDAFQK-GW22YGA-YWHLKB3-VOECXQV";};
       };
       folders = {
         "Books" = {
